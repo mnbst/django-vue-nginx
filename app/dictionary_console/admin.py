734f8241b829ec27cdfca96cea770cb3e86e20b6
mnbst <47024101+mnbst@users.noreply.github.com>
2019-12-16 19:08:37 +0900

    update

--- app/dictionary_console/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Word
-from .models import Caption
-from .models import Video
-
-admin.site.register(Word)
-admin.site.register(Caption)
-admin.site.register(Video)
--- app/frontend/src/components/menu/VideoViewer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div id="VideoViewer"></div>
-</template>
-
-<script>
-export default {
-    
-}
-</script>
