1b59e9c7e12ce2e10ceb30e685cbc1ffe6213b38
mnbst <j5kui0c@gmail.com>
2021-05-09 10:55:44 +0900

    delete: login form

--- django/app/urls.py
+++ django/app/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
-from .views import IndexView, LogOutView
+from .views import IndexView
+
 
 urlpatterns = [
     path(r"admin/", admin.site.urls),
     path(r"graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path(r"", IndexView.as_view(), name="index"),
-    path(r"logout/", LogOutView.as_view(), name="logout"),
 ]
